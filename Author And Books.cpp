d375d93e801c512597f9b695401c1550bf4f0364
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-02 09:43:05 +0530

    Create Author And Books.cpp

---
+++ Author And Books.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+
+
+vector<string> arrangeAuthors(vector<vector<string> > &s) {
+    vector<string> formattedData;
+
+    char bookLetter = 'A';
+
+    for (int i = 0; i < s.size(); i++) {
+        string authorName = s[i][0];
+        formattedData.push_back(to_string(i + 1) + ". " + authorName);
+
+        for (int j = 1; j < s[i].size(); j++) {
+            formattedData.push_back("    " + string(1, bookLetter) + ". " + s[i][j]);
+            bookLetter++;
+        }
+
+        bookLetter = 'A';
+    }
+
+    return formattedData;
+}
+
+
+
+
+
