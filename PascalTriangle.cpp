91034c9659603d41178f7d3cbbb214768c545bf4
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-06 19:20:12 +0530

    Create PascalTriangle.cpp

---
+++ PascalTriangle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+vector<long long int> generateRows(int rows){
+
+  vector <long long int> ansRow ;
+
+  long long ans = 1 ;
+  ansRow.push_back(1);
+
+  for(int col = 1 ; col < rows ; col++){
+
+    ans = ans * (rows - col) ;
+    ans = ans / col;
+    ansRow.push_back(ans);
+
+  }
+  return ansRow ;
+}
+
+vector<vector<long long int>> printPascal(int n) 
+
+{
+    vector<vector <long long int >> ans ;
+
+       for(int i = 1 ; i <= n ; i++){
+
+           ans.push_back(generateRows(i));
+
+       }
+    return ans ;
+}
