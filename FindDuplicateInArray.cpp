4b83a09440c825f2ce15fb6fefa56b62e97d995b
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-08 18:54:05 +0530

    Create FindDuplicateInArray.cpp

---
+++ FindDuplicateInArray.cpp
@@ -0,0 +1,11 @@
+int findDuplicate(vector<int> &arr) 
+{
+   int n = arr.size()-1;
+   n = ((n*(n+1))/2);
+    int sum =0;
+   for(int i=0;i<arr.size();i++)
+   {
+       sum+=arr[i];
+   }
+	return (sum-n);
+}
